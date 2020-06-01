9a5a3b0ee3c390f4fd998166ca3cebb462dd7233
011994 <011994@nttdata.com>
2020-06-01 19:39:00 +0530

    ui changes

--- src/app/shared/campaign.model.ts
+++ src/app/shared/campaign.model.ts
@@ -6,8 +6,9 @@ export class Campaign  {
        public longdesc : string,
        public startdate :string, public enddate :string,
        public main_logo: string,public slogan: string,
-       public footer_logo1: string,public footer_logo2: string
-      
+       public footer_logo1: string,public footer_logo2: string,
+       public contact1_name: string,public contact1_contact: string, 
+       public contact2_name: string,public contact2_contact: string
        ){}
 }
 
